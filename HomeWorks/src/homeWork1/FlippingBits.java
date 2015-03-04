089f0d65890257fcf408a37eff37a902b9d74144
Alexandr Lopoukhov <tigraride@gmail.com>
2015-03-04 14:47:12 +0300

    close Scanner stream

--- HomeWorks/src/homeWork1/FlippingBits.java
+++ HomeWorks/src/homeWork1/FlippingBits.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 public class FlippingBits {
 	public static void main(String[] arg) {
-		Scanner in = new Scanner(System.in);
-		int T = in.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
 		while (T-- > 0) {
 			// Multiply with longest unsigned 32bit int ( 2^32 or 1xFFFFFFFFFL
 			// or 4294967295L)
-			System.out.println(in.nextLong() ^ 4294967295L);
+			System.out.println(sc.nextLong() ^ 4294967295L);
 		}
+		sc.close();
 	}
 }
--- HomeWorks/src/homeWork1/HomeWork1.java
+++ HomeWorks/src/homeWork1/HomeWork1.java
@@ -47,6 +47,8 @@ public class HomeWork1 {
 			} else {
 				System.out.println("Неверная команда, повторите");
 			}
+
+			sc.close();
 		}
 	}
 
