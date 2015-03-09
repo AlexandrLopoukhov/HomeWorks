470f74e2c81c1747c67ee42e32988827a58bdb36
Alexandr Lopoukhov <tigraride@gmail.com>
2015-03-09 21:19:26 +0300

    add try\catch block

--- HomeWorks/src/homeWork1/HomeWork1.java
+++ HomeWorks/src/homeWork1/HomeWork1.java
@@ -22,38 +22,44 @@ public class HomeWork1 {
 
 		int steps = 0;
 
-		System.out
-				.println("Загадайте число от 0 до 100. Компьютер попробует отгадать его.");
+		System.out.println("Загадайте число от 0 до 100. "
+				+ "Компьютер попробует отгадать его.");
 
 		System.out.println(lastNumber);
 
-		while (sc.hasNext()) {
-			String userInput = sc.nextLine();
-			if (userInput.equals("less")) {
-				maxSeed = lastNumber;
-				lastNumber = rand.nextInt(maxSeed - minSeed) + minSeed;
-				System.out.println(lastNumber);
-				steps++;
-			} else if (userInput.equals("more")) {
-				minSeed = lastNumber + 1;
-				lastNumber = rand.nextInt(maxSeed - minSeed) + minSeed;
-				System.out.println(lastNumber);
-				steps++;
-			} else if (userInput.equals("quit")) {
-				break;
-			} else if (userInput.equals("bingo")) {
-				System.out.println("Компьютера угадала число за " + steps
-						+ " шагов");
-				break;
-			} else if (steps > 7) {
-				System.out
-						.println("Компьютеру не удалось угадать ваше число за 7 попыток. Попробуйте еще раз.");
-				break;
-			} else {
-				System.out.println("Неверная команда, повторите");
-			}
-		}
+		try {
+			while (sc.hasNext()) {
+				String userInput = sc.nextLine();
+				if (userInput.equals("less")) {
+					maxSeed = lastNumber;
+					lastNumber = rand.nextInt(maxSeed - minSeed) + minSeed;
+					System.out.println(lastNumber);
+					steps++;
+				} else if (userInput.equals("more")) {
+					minSeed = lastNumber + 1;
+					lastNumber = rand.nextInt(maxSeed - minSeed) + minSeed;
+					System.out.println(lastNumber);
+					steps++;
+				} else if (userInput.equals("quit")) {
+					break;
+				} else if (userInput.equals("bingo")) {
+					System.out.println("Компьютера угадала число за " + steps
+							+ " шагов");
+					break;
+				} else {
+					System.out.println("Неверная команда, повторите");
+				}
 
-		sc.close();
+				if (steps > 7) {
+					System.out.println("Компьютеру не удалось "
+							+ "угадать ваше число за 7 попыток. "
+							+ "Попробуйте еще раз.");
+					break;
+				}
+			}
+			sc.close();
+		} catch (Exception e) {
+			System.out.println("Вы уверены, что отвечали верно?");
+		}
 	}
 }
