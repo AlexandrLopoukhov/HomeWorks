ea6c99a9bb95b324429e8f72a8d69c3deac9bef3
Alexandr Lopoukhov <tigraride@gmail.com>
2015-03-09 21:03:27 +0300

    fix bag

--- HomeWorks/src/homeWork1/HomeWork1.java
+++ HomeWorks/src/homeWork1/HomeWork1.java
@@ -22,7 +22,8 @@ public class HomeWork1 {
 
 		int steps = 0;
 
-		System.out.println("Загадайте число от 0 до 100. Компьютер попробует отгадать его.");
+		System.out
+				.println("Загадайте число от 0 до 100. Компьютер попробует отгадать его.");
 
 		System.out.println(lastNumber);
 
@@ -45,15 +46,14 @@ public class HomeWork1 {
 						+ " шагов");
 				break;
 			} else if (steps > 7) {
-				System.out.println("Компьютеру не удалось угадать ваше число за 7 попыток. Попробуйте еще раз.");
+				System.out
+						.println("Компьютеру не удалось угадать ваше число за 7 попыток. Попробуйте еще раз.");
 				break;
 			} else {
-			}
 				System.out.println("Неверная команда, повторите");
 			}
-
-			sc.close();
 		}
-	}
 
+		sc.close();
+	}
 }
